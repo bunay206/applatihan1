01f6a5fe3da00b261d32431d68b83501fab1c5c2
Your Name <you@example.com>
2021-02-09 03:20:13 +0700

    main

--- App.js
+++ App.js
@@ -47,7 +47,7 @@ const App: () => React$Node = () => {
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Are you winning son?</Text>
+              <Text style={styles.sectionTitle}>Are you winning son?????</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
